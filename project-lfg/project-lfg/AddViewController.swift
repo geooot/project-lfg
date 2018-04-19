20bfc12d8bc079ce321bad46b30b4eb2b445a290
Tosh Witherspoon <toshtwitherspoon@gmail.com>
2018-04-19 14:30:12 -0500

    pls

--- project-lfg/project-lfg/AddViewController.swift
+++ project-lfg/project-lfg/AddViewController.swift
@@ -9,20 +9,55 @@
 import Foundation
 import Eureka
 
+let getRanks = ["None": [], "League of Legends": ["Unranked", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Challenger"], "Rainbow Six Siege": ["Unranked", "Copper", "Bronze", "Silver", "Gold", "Platinum", "Diamond"], "Overwatch": ["Unranked", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Master", "Grandmaster", "Top 500"]]
+
 class AddViewController: FormViewController {
     
-    
+    var GameName:String = ""
+    var GameRank:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         form +++ Section("Game Choice")
-            <<< ActionSheetRow<String>() {
+            <<< ActionSheetRow<String>()
+                {
+                $0.tag = "GamePicker"
                 $0.title = "Game Picker"
                 $0.selectorTitle = "Pick a Game"
-                $0.options = ["None","Overwatch","League of Legends","Rainbow Six"]
-                $0.tag = "GameName"
+                $0.options = ["None","Overwatch","League of Legends","Rainbow Six Siege"]
                 $0.value = "None"
+                GameName = $0.value!
+                }.onChange{ row in
+                    self.GameName = row.value!;
+                    self.addRankings()
+                    print(self.GameName)
+                }
+        form +++ Section("Time Played")
+            <<< PhoneRow() {
+                $0.title = "Hours Played"
+                $0.placeholder = "Hours Played"
                 
         }
+        form +++ Section("Rankings")
+        
+    }
+    
+    func addRankings()
+    {
+        form.allSections[2].removeAll()
+        form.allSections[2]
+            <<< ActionSheetRow<String>() { row in
+                row.tag = "GameRank"
+                row.title = "Rankings"
+                row.selectorTitle = "Pick your rank"
+                row.options = getRanks[self.GameName]!
+                row.value = "None"
+                GameRank = row.value!
+                }.onChange{ row in
+                    self.GameRank = row.value!;
+                    print(self.GameRank)
+        }
+       
     }
 }
+
